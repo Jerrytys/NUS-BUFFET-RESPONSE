42d153a120a945c4da3e49f796592875dc2208a4
Jerrytys <jerrytys03@gmail.com>
2025-07-21 18:19:47 +0800

    Edited Start Page

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, TouchableOpacity, StyleSheet, Text, View, Image, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image style={styles.logo}source={require('./assets/nuslogo.png')}/>   
+      
+      <Text style={styles.title}>NUS FOOD RUNNER</Text>
+
+      <TouchableOpacity style={styles.loginButton}>
+        <Text style={styles.loginText}>Login</Text>
+      </TouchableOpacity>
+
+      <View style={styles.login}>
+        <Text style={styles.signUpText}>Don't have an account?</Text>
+        <Button title='Sign Up'/>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +25,48 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF8AC',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  title: {
+    fontSize: 28,
+    fontWeight: 700,
+    marginBottom: 20
+  },
+  
+  logo: {
+    height: 240,
+    width: 240,
+    marginBottom: 30,
+  },
+  
+  login: {
+    flexDirection:'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  loginButton: {
+    backgroundColor: '#4781FF',
+    borderRadius: 25,
+    margin: 20,
+  },
+  loginText: {
+    paddingLeft: 120,
+    paddingRight: 120,
+    paddingTop: 5,
+    paddingBottom: 5,
+    textAlign: 'center',
+    fontSize: 20
+  },
+
+  SignUpButton:{
+    
+  },
+
+  signUpText: {
+    fontSize: 16
+  }
 });
